a253688c2ffff493455a307711d1ba309f12df1b
m4ch374 <spiner47@gmail.com>
2023-06-19 16:31:29 +1000

    Db: updated db

--- components/book_card.js
+++ components/book_card.js
@@ -2,7 +2,7 @@ import '../styles/book_card.css'
 
 const BookCard = (meta_data) => {
   return `
-    <div class="book-card">
+    <div class="book-card" id="book-${meta_data.id}">
       <div class="img-wrapper">
         <img alt="cover" src="${meta_data.cover}" />
       </div>
