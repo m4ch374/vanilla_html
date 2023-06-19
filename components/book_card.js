9c022249951ff645f8e3aa982dd49e7c543980bb
m4ch374 <spiner47@gmail.com>
2023-06-19 19:16:51 +1000

    Dev: adding book system

--- components/book_card.js
+++ components/book_card.js
@@ -3,6 +3,7 @@ import '../styles/book_card.css'
 const BookCard = (meta_data) => {
   return `
     <div class="book-card" id="book-${meta_data.id}">
+      <button class="book-card-select" id="select-${meta_data.id}">+</button>
       <a href="../pages/book.html?book=${meta_data.id}">
         <div class="img-wrapper">
           <img alt="cover" src="${meta_data.cover}" />
--- scripts/catalogue.js
+++ scripts/catalogue.js
@@ -1,8 +1,34 @@
 import BookCard from "../components/book_card"
 import data from '/database.json'
+import { bookSelected, selectElemHandler } from "./shopping_cart_hook"
 
 const book_catalogue = document.querySelector("#book-catalogue")
 
 data.books.forEach(b => {
   book_catalogue.innerHTML += BookCard(b)
+})
+
+const changeBtn = (btn) => {
+  const btnId = btn.id
+  const bookId = btnId.split("-")[1]
+
+  if (!bookSelected(bookId)) {
+    btn.textContent = "+"
+    btn.classList.add("book-card-add")
+    btn.classList.remove("book-card-remove")
+  } else {
+    btn.textContent = "-"
+    btn.classList.add("book-card-remove")
+    btn.classList.remove("book-card-add")
+  }
+}
+
+document.querySelectorAll(".book-card-select").forEach(x => {
+  x.addEventListener("click", () => {
+    const bookId = x.id.split("-")[1]
+    selectElemHandler(bookId)
+    changeBtn(x)
+  })
+
+  changeBtn(x)
 })
